525f36a7a59ee9fa8d929804c7cd7a424b1cecfc
YousraKhan <yousra.khan97@hotmail.com>
2019-07-13 01:33:50 +0500

    Updates

---
+++ precache-manifest.dca0ebac3687bc36402520b61951db4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1d845339e1ad83899bb45306c2cc5a",
+    "url": "/mycalculator/index.html"
+  },
+  {
+    "revision": "588b323345433572bace",
+    "url": "/mycalculator/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "4b9617a191c572be2329",
+    "url": "/mycalculator/static/css/main.6ebf1340.chunk.css"
+  },
+  {
+    "revision": "588b323345433572bace",
+    "url": "/mycalculator/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "4b9617a191c572be2329",
+    "url": "/mycalculator/static/js/main.71b0093e.chunk.js"
+  },
+  {
+    "revision": "8b33e20d773e5812c7b4",
+    "url": "/mycalculator/static/js/runtime~main.c0a9aa15.js"
+  }
+]);
